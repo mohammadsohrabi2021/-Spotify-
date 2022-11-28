b8433a13f367ecd73499189846aa0c45442fd845
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-11-28 11:43:34 +0330

    create data project

---
+++ Data/dataMain.js
@@ -0,0 +1,32 @@
+import img1 from '../image/Rectangle8.png'
+import img2 from '../image/Rectangle2.png'
+import img3 from '../image/Rectangle.png'
+import img4 from '../image/Rectangle7.png'
+import img5 from '../image/Ellipse 6.png'
+import img6 from '../image/Ellipse 4.png'
+import img7 from '../image/Ellipse 5.png'
+import img8 from '../image/Rectangle6.png'
+import img9 from '../image/Rectangle3.png'
+import img10 from '../image/Rectangle4.png'
+import img11 from '../image/Rectangle1.png'
+import img12 from '../image/Ellipse 1.png'
+import img13 from '../image/Ellipse 3.png'
+import img14 from '../image/Ellipse 2.png'
+export const list1 =[
+    {id:1,image:img1,title:'Hot Hits USA',dis:'The hottest tracks in the United States'},
+    {id:2,image:img2,title:'Hot Hits USA',dis:'The hottest tracks in the United States'},
+    {id:3,image:img3,title:'Hot Hits USA',dis:'The hottest tracks in the United States'},
+    {id:4,image:img4,title:'Destiny Rogers',dis:'Artist'},
+    {id:5,image:img5,title:'Destiny Rogers',dis:'Artist'},
+    {id:6,image:img6,title:'Destiny Rogers',dis:'Artist'},
+    {id:7,image:img7,title:'Pop Right Now',dis:'The hottest tracks in the United States'},
+]
+export const list2=[
+    {id:1,image:img8,title:'Pop Right Now',dis:'The hottest tracks in the United States'},
+    {id:2,image:img9,title:'Pop Right Now',dis:'Artist'},
+    {id:3,image:img10,title:'Destiny Rogers',dis:'The hottest tracks in the United States'},
+    {id:4,image:img11,title:'Destiny Rogers',dis:'Artist'},
+    {id:5,image:img12,title:'Destiny Rogers',dis:'The hottest tracks in the United States'},
+    {id:6,image:img13,title:'Pop Right Now',dis:'The hottest tracks in the United States'},
+    {id:7,image:img14,title:'Pop Right Now',dis:'The hottest tracks in the United States'},
+]
