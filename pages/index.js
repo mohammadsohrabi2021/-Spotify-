49440cd93216c11e92a520a4c43671e64fd01996
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-11-28 11:45:31 +0330

    add main route

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import { Grid } from '@mui/material'
+import { Grid } from "@mui/material";
+import Sidebar from "../Component/Sidebar";
+import Main from "../Component/Home";
+import Footer from "../Component/Footer";
 
 export default function Home() {
   return (
-    <Grid>
-     
+    <Grid item xs={12} container>
+      <Grid item xs={12} display={"flex"} justifyContent={"space-between"}>
+        <Sidebar />
+        <Main />
+      </Grid>
+      <Grid item sx={{ position: "relative" }}>
+        <Footer />
+      </Grid>
     </Grid>
-  )
+  );
 }
